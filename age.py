61e84c68d9f4e1f134aadfd7770fd529ba0ab1e2
Ranisolanki <59246445+rani-solanki@users.noreply.github.com>
2020-11-16 13:42:06 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,19 @@
+# # # tring to wight code for age.
+# age=int(input("enter the age"))
+# if age >= 6:
+#     print("you can go school")
+# if age >= 18:
+#     print("you can voting now")
+# if age >= 21:
+#     print(" you can get marry")
+# else:
+#     print("you can do those you want")
+
+# a=10
+# i=1
+# # input=input("enter the ")
+# while(i<=10):
+#     print("2 *",i,"=",5 *i)
+#     i=i+1
+
+
