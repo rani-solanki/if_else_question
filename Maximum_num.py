2d43ac26aafa4b58141ebbfbb6716b3007391960
Ranisolanki <59246445+rani-solanki@users.noreply.github.com>
2021-01-16 23:25:18 +0530

    Add files via upload

---
+++ Maximum_num.py
@@ -0,0 +1,9 @@
+num1 = int(input("entre the number"))
+num2 = int(input("enter the n umber"))
+num3 = int(input("entre the number"))
+if (num1<num2>num3):
+    print(num2)
+elif (num2<num3>num1):
+    print(num3)
+else:
+    print(num1)
